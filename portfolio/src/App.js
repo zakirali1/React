8dcdf023b3330ff8a6c48ad67e7c980d47e4e949
Zak <zakirali1@hotmail.co.uk>
2023-03-14 13:55:45 +0000

    issues with links

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import Projects from './Projects2';
@@ -27,6 +27,7 @@ function App() {
           <Route path="/Projects" element={<Projects />} />
           <Route path='/Projects/:id' element={<ProjectDetails />} />
           <Route path='/Contacts' element={<Contacts />} />
+          <Route path="*" element={<Navigate to='/' />} />
         </Routes>
         </Router>
     </div>
--- portfolio/src/Projects2.js
+++ portfolio/src/Projects2.js
@@ -31,7 +31,7 @@ function Projects2() {
   const ProjectDetails = ({ project, index }) => (
     
     <div className="project-card">
-      <Col key={project.id} md={6} className="mb-4 justify-content-md-center">
+      <Col key={project.id} md={6} className="mb-4">
         <NavLink to={`/projects/${project.id}`}>
           <Card>
             <Card.Img variant="top" src={genImage(project.id)} />
